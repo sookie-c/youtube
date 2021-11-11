67e6bf2e2ec6141e3547a055b926e897bdc22219
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-11 22:16:01 +1030

    Search and add youtube service

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './search_header.module.css';
 
-const SearchHeader = (props) => (
-  <header className={styles.header}>
-    <div className={styles.logo}>
-      <img src="images/logo.png" alt="youtube logo" className={styles.img} />
-      <h1 className={styles.title}>Youtube</h1>
-    </div>
-    <input type="search" className={styles.input} placeholder="Search" />
-    <button className={styles.button}>
-      <img
-        className={styles.buttonImg}
-        src="images/search.png"
-        alt="search button"
+const SearchHeader = ({ onSearch }) => {
+  const inputRef = useRef();
+
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
+  };
+
+  const onClick = () => {
+    console.log('onClick');
+    handleSearch();
+  };
+
+  const onKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src="images/logo.png" alt="youtube logo" className={styles.img} />
+        <h1 className={styles.title}>Youtube</h1>
+      </div>
+      <input
+        className={styles.input}
+        ref={inputRef}
+        type="search"
+        placeholder="Search"
+        onKeyPress={onKeyPress}
       />
-    </button>
-  </header>
-);
+      <button className={styles.button} type="submit" onClick={onClick}>
+        <img
+          className={styles.buttonImg}
+          src="images/search.png"
+          alt="search button"
+        />
+      </button>
+    </header>
+  );
+};
 
 export default SearchHeader;
