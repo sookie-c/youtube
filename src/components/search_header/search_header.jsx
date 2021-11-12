763ca586e55bc73fba9631cf40793f5e2f5aa40b
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-12 22:26:02 +1030

    Responsive header and add profile pic

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -25,20 +25,27 @@ const SearchHeader = memo(({ onSearch, onLogoClick }) => {
         <img src="images/logo.png" alt="youtube logo" className={styles.img} />
         <h1 className={styles.title}>Youtube</h1>
       </div>
-      <input
-        className={styles.input}
-        ref={inputRef}
-        type="search"
-        placeholder="Search"
-        onKeyPress={onKeyPress}
-      />
-      <button className={styles.button} type="submit" onClick={onClick}>
-        <img
-          className={styles.buttonImg}
-          src="images/search.png"
-          alt="search button"
+      <div className={styles.search}>
+        <input
+          className={styles.input}
+          ref={inputRef}
+          type="search"
+          placeholder="Search"
+          onKeyPress={onKeyPress}
         />
-      </button>
+        <button className={styles.button} type="submit" onClick={onClick}>
+          <img
+            className={styles.buttonImg}
+            src="images/search.png"
+            alt="search button"
+          />
+        </button>
+      </div>
+      <img
+        className={styles.profile}
+        src="images/profile.png"
+        alt="profile picture"
+      />
     </header>
   );
 });
